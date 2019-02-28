2d3910321e208c7735734edd2bfa68f8f5124e9f
Enihsuns <enihsunsyxy@gmail.com>
2019-02-28 13:42:53 -0500

    Slide class

---
+++ src/Slide.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Slide {
+    List<Integer> tags;
+    List<Integer> sourcePhotos;
+
+    public Slide(List<Integer> tags, List<Integer> sourcePhotos) {
+        this.tags = tags;
+        this.sourcePhotos = sourcePhotos;
+    }
+}
