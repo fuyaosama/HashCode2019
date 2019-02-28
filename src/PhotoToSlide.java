149ec941397dbfa6ec2ae448cb5ab9ae71ae5ea0
fuyaosama <358767643@qq.com>
2019-02-28 14:15:35 -0500

    finish input

--- src/PhotoToSlide.java
+++ src/PhotoToSlide.java
@@ -5,21 +5,33 @@ import java.util.*;
 
 
 public class PhotoToSlide {	
-	
+	List<Photo> photolist = new ArrayList();
 	List<Slide> slidelist = new ArrayList();
+	HashMap<String,Integer> tagindexmap = new HashMap();
+	int tagcount = 0;
 	void parse(String filename) throws FileNotFoundException {
 		File file = new File(filename);
 		Scanner sc = new Scanner(file);
 		int photonumber = sc.nextInt();
+		
 		for(int i=0;i<photonumber;i++) {
 			String HorV = sc.next();
 			int tagnum = sc.nextInt();
-			List<String> tags = new ArrayList();
+			List<Integer> tags = new ArrayList();
 			for(int j=0;j<tagnum;j++) {
-				tags.add(sc.next());
+				String tag = sc.next();
+				if(tagindexmap.containsKey(tag)) {
+					tags.add(tagindexmap.get(tag));
+				}
+				else {
+					tags.add(tagcount);
+					tagindexmap.put(tag, tagcount++);
+					
+				}
+				
 			}
 			
-			
+			photolist.add(new Photo(tags, HorV.equals("H")));
 		}
 		sc.close();
 	}
